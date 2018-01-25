15b2e57b8ae2130a267eff40cf308b1dfadb9490
Ron Dekker <redkenrok@gmail.com>
2018-01-25 15:29:01 +0100

    Removed Wit.ai, Google cloud languages, added string-similarity module

--- app/scripts/input.js
+++ app/scripts/input.js
@@ -84,11 +84,6 @@ const input = {};
 			},
 			interimResults: false
 		}
-		// Google Cloud Language.
-		const Language = require('@google-cloud/language');
-		const language = new Language.LanguageServiceClient({
-			keyFilename: KEYPATH_GOOGLECLOUD
-		});
 		
 		// Record function.
 		input.record = function(buffer) {
@@ -96,79 +91,18 @@ const input = {};
 			let stream = speech.streamingRecognize(speechRequest)
 				.on('error', console.error)
 				.on('data', function(data) {
-					console.log(`Transcription: ${data.results[0].alternatives[0].transcript}`);
-					
-					// Start analyzing the entities of the transcript.
-					language.analyzeEntities({
-						document: {
-							content: data.results[0].alternatives[0].transcript,
-							type: 'PLAIN_TEXT'
-						}
-					})
-						.then(function(results) {
-							let entities = results[0].entities;
-							
-							// log results.
-							entities.forEach(entity => {
-								console.log(entity.name);
-								console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
-								if (entity.metadata && entity.metadata.wikipedia_url) {
-									console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}$`);
-								}
-							});
-							
-							// Invoke received event.
-							input.element.dispatchEvent(
-								new CustomEvent('received', {
-									detail : {
-										response: results[0].entities
-									}})
-								);
-						})
-						.catch(function(error) {
-							console.error('ERROR', error);
-						});
+					// Invoke received event.
+					input.element.dispatchEvent(
+						new CustomEvent('received', {
+							detail : {
+								response: data.results[0].alternatives[0].transcript
+							}})
+						);
 				});
 			// Start streaming audio to web stream.
 			audioRecorder.start().stream()
 				.pipe(stream);
 		};
-	}
-	// Wit.ai
-	else if (fs.existsSync(KEYPATH_WITAI)) {
-		// Get the keys.
-		let keys = JSON.parse(fs.readFileSync(KEYPATH_WITAI, 'utf8'));
-		
-		// Wit.ai speech wrapper.
-		const WitSpeech = require('witspeech');
-		const witSpeech = new WitSpeech(
-			// Prefer the client key over the server key.
-			keys.client != null ? keys.client : keys.server,
-			// Get API version from configuration.
-			config.wit.version
-		);
-		
-		// Record function.
-		input.record = function(buffer) {
-			// Create web request.
-			let witSpeechRequest = witSpeech.request('audio/wav', {}, function(error, response) {
-				if (error) {
-					console.error('Error', error, 'Code', response.code, ', ', response.error);
-					return;
-				}
-				
-				// Invoke received event.
-				input.element.dispatchEvent(
-					new CustomEvent('received', {
-						detail : {
-							response: response
-						}})
-					);
-			});
-			// Start audio recorder, and pipe audio stream to the web request.
-			audioRecorder.start().stream()
-				.pipe(witSpeechRequest);
-		}
 	}
 	// If no service configured display a warning message.
 	else {
